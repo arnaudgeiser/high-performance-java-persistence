dc8580b88913cec177365baef75db274b9568599
Arnaud Geiser <arnaudgeiser@gmail.com>
2020-01-09 18:24:42 +0100

    Added PostComment to test Query Cache Plan with IN queries and entities

--- core/src/test/java/com/vladmihalcea/book/hpjp/hibernate/query/InQueryTest.java
+++ core/src/test/java/com/vladmihalcea/book/hpjp/hibernate/query/InQueryTest.java
@@ -1,11 +1,10 @@
 package com.vladmihalcea.book.hpjp.hibernate.query;
 
 import com.vladmihalcea.book.hpjp.util.AbstractTest;
-import com.vladmihalcea.book.hpjp.util.providers.Database;
+import java.util.stream.IntStream;
 import org.junit.Test;
 
 import javax.persistence.*;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Properties;
@@ -18,78 +17,153 @@ import static org.junit.Assert.assertEquals;
  */
 public class InQueryTest extends AbstractTest {
 
-    @Override
-    protected Class<?>[] entities() {
-        return new Class<?>[]{
-            Post.class
-        };
-    }
+  @Override
+  protected Class<?>[] entities() {
+    return new Class<?>[]{
+        Post.class,
+        PostComment.class
+    };
+  }
 
-    @Override
-    protected void additionalProperties(Properties properties) {
-        properties.put("hibernate.jdbc.batch_size", "50");
-        properties.put("hibernate.order_inserts", "true");
-        properties.put("hibernate.query.in_clause_parameter_padding", "true");
-    }
+  @Override
+  protected void additionalProperties(Properties properties) {
+    properties.put("hibernate.jdbc.batch_size", "50");
+    properties.put("hibernate.order_inserts", "true");
+    properties.put("hibernate.query.in_clause_parameter_padding", "true");
+  }
 
-    @Test
-    public void testPadding() {
-        doInJPA(entityManager -> {
-            for (int i = 1; i <= 15; i++) {
-                Post post = new Post();
-                post.setId(i);
-                post.setTitle(String.format("Post no. %d", i));
+  @Test
+  public void testPadding() {
+    doInJPA(entityManager -> {
+      List<Post> posts = IntStream.range(1, 16)
+          .mapToObj(id -> {
+            Post post = new Post();
+            post.setId(id);
+            post.setTitle(String.format("Post no. %d", id));
+            return post;
+          })
+          .collect(Collectors.toList());
 
-                entityManager.persist(post);
-            }
-        });
+      List<PostComment> postComments = IntStream.range(0, posts.size())
+          .mapToObj(i -> {
+            int id = i + 1;
+            PostComment postComment = new PostComment();
+            postComment.setId(id);
+            postComment.setComment(String.format("Post comment. %d", id));
+            postComment.setPost(posts.get(i));
+            return postComment;
+          })
+          .collect(Collectors.toList());
 
-        doInJPA(entityManager -> {
-            assertEquals(3, getPostByIds(entityManager, 1, 2, 3).size());
-            assertEquals(4, getPostByIds(entityManager, 1, 2, 3, 4).size());
-            assertEquals(5, getPostByIds(entityManager, 1, 2, 3, 4, 5).size());
-            assertEquals(6, getPostByIds(entityManager, 1, 2, 3, 4, 5, 6).size());
-        });
-    }
+      posts.forEach(entityManager::persist);
+      postComments.forEach(entityManager::persist);
+    });
 
-    List<Post> getPostByIds(EntityManager entityManager, Integer... ids) {
-        return entityManager.createQuery(
-            "select p " +
+    doInJPA(entityManager -> {
+      List<Post> postsSize3 = getPostByIds(entityManager, 1, 2, 3);
+      List<PostComment> postsCommentsOfSize3 = getPostCommentsByPosts(entityManager, postsSize3);
+      assertEquals(3, postsSize3.size());
+      assertEquals(3, postsCommentsOfSize3.size());
+
+      List<Post> postsSize4 = getPostByIds(entityManager, 1, 2, 3, 4);
+      assertEquals(4, postsSize4.size());
+      assertEquals(4, getPostCommentsByPosts(entityManager, postsSize4).size());
+
+      List<Post> postsSize5 = getPostByIds(entityManager, 1, 2, 3, 4, 5);
+      assertEquals(5, postsSize5.size());
+      assertEquals(5, getPostCommentsByPosts(entityManager, postsSize5).size());
+
+      List<Post> postsSize6 = getPostByIds(entityManager, 1, 2, 3, 4, 5, 6);
+      assertEquals(6, postsSize6.size());
+      assertEquals(6, getPostCommentsByPosts(entityManager, postsSize6).size());
+    });
+  }
+
+  List<PostComment> getPostCommentsByPosts(EntityManager entityManager, List<Post> posts) {
+    return entityManager.createQuery(
+        "select pc " +
+            "from PostComment pc " +
+            "where pc.post in :posts", PostComment.class)
+        .setParameter("posts", posts)
+        .getResultList();
+  }
+
+  List<Post> getPostByIds(EntityManager entityManager, Integer... ids) {
+    return entityManager.createQuery(
+        "select p " +
             "from Post p " +
             "where p.id in :ids", Post.class)
         .setParameter("ids", Arrays.asList(ids))
         .getResultList();
+  }
+
+  @Entity(name = "Post")
+  @Table(name = "post")
+  public static class Post {
+
+    @Id
+    private Integer id;
+
+    private String title;
+
+    public Post() {
     }
 
-    @Entity(name = "Post")
-    @Table(name = "post")
-    public static class Post {
-
-        @Id
-        private Integer id;
-
-        private String title;
-
-        public Post() {}
-
-        public Post(String title) {
-            this.title = title;
-        }
-
-        public Integer getId() {
-            return id;
-        }
-
-        public void setId(Integer id) {
-            this.id = id;
-        }
-
-        public String getTitle() {
-            return title;
-        }
-
-        public void setTitle(String title) {
-            this.title = title;
-        }
+    public Post(String title) {
+      this.title = title;
     }
+
+    public Integer getId() {
+      return id;
+    }
+
+    public void setId(Integer id) {
+      this.id = id;
+    }
+
+    public String getTitle() {
+      return title;
+    }
+
+    public void setTitle(String title) {
+      this.title = title;
+    }
+  }
+
+  @Entity(name = "PostComment")
+  @Table(name = "postcomment")
+  public static class PostComment {
+
+    @Id
+    private Integer id;
+
+    @ManyToOne
+    private Post post;
+
+    private String comment;
+
+    public Integer getId() {
+      return id;
+    }
+
+    public void setId(Integer id) {
+      this.id = id;
+    }
+
+    public Post getPost() {
+      return post;
+    }
+
+    public void setPost(Post post) {
+      this.post = post;
+    }
+
+    public String getComment() {
+      return comment;
+    }
+
+    public void setComment(String comment) {
+      this.comment = comment;
+    }
+  }
 }
